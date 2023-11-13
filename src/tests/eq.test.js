b3c7265d87d09ce92ca94f226537349bdd0eae79
Ronja <ronja.lipsonen@tuni.fi>
2023-11-13 14:23:59 +0200

    Eq tests created

---
+++ src/tests/eq.test.js
@@ -0,0 +1,40 @@
+import eq from '../eq.js';
+
+const object = { a: 1 };
+const other = { a: 1 };
+
+describe('eq', () => {
+  it('should determine that objects are equal', () => {
+    expect(eq(object, object)).toBe(true);
+  });
+});
+
+describe('eq', () => {
+  it('should determine that objects are not equal', () => {
+    expect(eq(object, other)).toBe(false);
+  });
+});
+
+describe('eq', () => {
+  it('should determine that strings are equal', () => {
+    expect(eq('a', 'a')).toBe(true);
+  });
+});
+
+describe('eq', () => {
+  it('should determine that values are not equal', () => {
+    expect(eq('a', Object('a'))).toBe(false);
+  });
+});
+
+describe('eq', () => {
+  it('should determine that chars are not equal', () => {
+    expect(eq('a', 'b')).toBe(false);
+  });
+});
+
+describe('eq', () => {
+  it('should determine that empty values are equal', () => {
+    expect(eq(NaN, NaN)).toBe(true);
+  });
+});
