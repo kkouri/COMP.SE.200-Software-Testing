ae2b67502f6f10345ef97402336cb839857968eb
Ronja <ronja.lipsonen@tuni.fi>
2023-11-15 15:04:36 +0200

    Test syntax corrected.

--- src/tests/get.test.js
+++ src/tests/get.test.js
@@ -6,15 +6,11 @@ describe('get', () => {
   it("return 3 as path's value for object 1", () => {
     expect(get(object, 'a[0].b.c')).toBe(3);
   });
-});
 
-describe('get', () => {
   it("return 3 as path's value for object 2", () => {
     expect(get(object, ['a', '0', 'b', 'c'])).toBe(3);
   });
-});
 
-describe('get', () => {
   it("return default as path's value for object", () => {
     expect(get(object, 'a.b.c', 'default')).toBe('default');
   });
