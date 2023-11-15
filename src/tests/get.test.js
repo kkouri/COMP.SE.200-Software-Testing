2459e097d01384ded2788899cb060f123dfa66ff
Ronja <ronja.lipsonen@tuni.fi>
2023-11-15 14:36:52 +0200

    Get tests created.

---
+++ src/tests/get.test.js
@@ -0,0 +1,21 @@
+import get from '../get.js';
+
+const object = { a: [{ b: { c: 3 } }] };
+
+describe('get', () => {
+  it("return 3 as path's value for object 1", () => {
+    expect(get(object, 'a[0].b.c')).toBe(3);
+  });
+});
+
+describe('get', () => {
+  it("return 3 as path's value for object 2", () => {
+    expect(get(object, ['a', '0', 'b', 'c'])).toBe(3);
+  });
+});
+
+describe('get', () => {
+  it("return default as path's value for object", () => {
+    expect(get(object, 'a.b.c', 'default')).toBe('default');
+  });
+});
