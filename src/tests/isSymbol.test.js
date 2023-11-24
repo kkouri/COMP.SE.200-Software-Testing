1377fe469781c34b31335521347892bbbf92ed65
Ronja <ronja.lipsonen@tuni.fi>
2023-11-24 15:35:54 +0200

    More tests added.

--- src/tests/isSymbol.test.js
+++ src/tests/isSymbol.test.js
@@ -3,9 +3,18 @@ import isSymbol from '../isSymbol.js';
 describe('isSymbol', () => {
   it('should check that value is symbol', () => {
     expect(isSymbol(Symbol.iterator)).toBe(true);
+    expect(isSymbol(Symbol('abc'))).toBe(true);
+    expect(isSymbol(Symbol.for('foo'))).toBe(true);
   });
 
   it('should check that value is not symbol', () => {
     expect(isSymbol('abc')).toBe(false);
+    expect(isSymbol(1)).toBe(false);
+    expect(isSymbol(NaN)).toBe(false);
+    expect(isSymbol(null)).toBe(false);
+    expect(isSymbol(undefined)).toBe(false);
+    expect(isSymbol(true)).toBe(false);
+    expect(isSymbol(false)).toBe(false);
+    expect(isSymbol({})).toBe(false);
   });
 });
