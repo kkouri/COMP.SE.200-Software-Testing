6aaed5dcee3b3c6c4de592897a251d86c4821a38
Ronja <ronja.lipsonen@tuni.fi>
2023-11-24 15:32:51 +0200

    Isempty tests added.

--- src/tests/isEmpty.test.js
+++ src/tests/isEmpty.test.js
@@ -1,27 +1,18 @@
 import isEmpty from '../isEmpty.js';
 
 describe('isEmpty', () => {
-  it('should check that value is empty 1', () => {
+  it('should check that value is empty', () => {
     expect(isEmpty(null)).toBe(true);
-  });
-
-  it('should check that value is empty 2', () => {
     expect(isEmpty(true)).toBe(true);
-  });
-
-  it('should check that value is empty 3', () => {
     expect(isEmpty(1)).toBe(true);
+    expect(isEmpty([])).toBe(true);
+    expect(isEmpty({})).toBe(true);
+    expect(isEmpty('')).toBe(true);
   });
 
-  it('should check that value is not empty 1', () => {
+  it('should check that value is not empty', () => {
     expect(isEmpty([1, 2, 3])).toBe(false);
-  });
-
-  it('should check that value is not empty 2', () => {
     expect(isEmpty('abc')).toBe(false);
-  });
-
-  it('should check that value is not empty 3', () => {
     expect(isEmpty({ a: 1 })).toBe(false);
   });
 });
