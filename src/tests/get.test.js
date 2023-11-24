37568f8211a1d7ec455a76a0afe9160051045bb6
Ronja <ronja.lipsonen@tuni.fi>
2023-11-24 13:40:42 +0200

    More tests added for get

--- src/tests/get.test.js
+++ src/tests/get.test.js
@@ -3,15 +3,26 @@ import get from '../get.js';
 const object = { a: [{ b: { c: 3 } }] };
 
 describe('get', () => {
-  it("return 3 as path's value for object 1", () => {
+  it('should return path value for object', () => {
     expect(get(object, 'a[0].b.c')).toBe(3);
-  });
-
-  it("return 3 as path's value for object 2", () => {
     expect(get(object, ['a', '0', 'b', 'c'])).toBe(3);
+    expect(get(object, ['a', '0', 'b', 'c'], 'default')).toBe(3);
   });
 
-  it("return default as path's value for object", () => {
+  it("should return default as path's value for object", () => {
     expect(get(object, 'a.b.c', 'default')).toBe('default');
+  });
+
+  it('should return undefined as path value for object', () => {
+    expect(get(object, 'a.b.c')).toBe(undefined);
+  });
+
+  it('should return path value for array', () => {
+    expect(get(['a', 'b', 'c'], '0')).toBe('a');
+    expect(get(['a', 'b', 'c'], '1')).toBe('b');
+  });
+
+  it("should return default as path's value for array", () => {
+    expect(get(['a', 'b', 'c'], '3', 'default')).toBe('default');
   });
 });
