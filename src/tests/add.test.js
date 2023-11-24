4ff52d246955232983fa7a233e882144c3421597
Ronja <ronja.lipsonen@tuni.fi>
2023-11-24 12:34:02 +0200

    More functions for add created.

--- src/tests/add.test.js
+++ src/tests/add.test.js
@@ -1,1 +1,17 @@
+describe('add', () => {
+  it('should add two positive numbers', () => {
+    expect(add(1, 2)).toBe(3);
+  });
 
+  it('should add positive and negative numbers', () => {
+    expect(add(1, -2)).toBe(-1);
+  });
+
+  it('should add two negative numbers', () => {
+    expect(add(-1, -2)).toBe(-3);
+  });
+
+  it('should add a negative number and zero', () => {
+    expect(add(-1, 0)).toBe(-1);
+  });
+});
