b2707519b53e0b0331daf66b18e319c472c10280
Ronja Lipsonen <ronetsu@dyn-157-34.public.tut.fi>
2023-12-04 13:00:51 +0200

    Added new lines to isEmpty

--- src/tests/isEmpty.test.js
+++ src/tests/isEmpty.test.js
@@ -1,18 +1,33 @@
 import isEmpty from '../isEmpty.js';
 
 describe('isEmpty', () => {
-  it('should check that value is empty', () => {
-    expect(isEmpty(null)).toBe(true);
-    expect(isEmpty(true)).toBe(true);
+  it('should check that string is empty', () => {
+    expect(isEmpty('')).toBe(true);
+  });
+
+  it('should check that number is empty', () => {
     expect(isEmpty(1)).toBe(true);
+  });
+
+  it('should check that container is empty', () => {
     expect(isEmpty([])).toBe(true);
     expect(isEmpty({})).toBe(true);
-    expect(isEmpty('')).toBe(true);
+  });
+
+  it('should check that boolean is empty', () => {
+    expect(isEmpty(false)).toBe(true);
+    expect(isEmpty(true)).toBe(true);
+  });
+
+  it('should check that empty is empty', () => {
+    expect(isEmpty(null)).toBe(true);
+    expect(isEmpty(NaN)).toBe(true);
   });
 
   it('should check that value is not empty', () => {
     expect(isEmpty([1, 2, 3])).toBe(false);
     expect(isEmpty('abc')).toBe(false);
+    expect(isEmpty(' ')).toBe(false);
     expect(isEmpty({ a: 1 })).toBe(false);
   });
 });
