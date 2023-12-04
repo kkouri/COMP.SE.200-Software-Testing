068fbae0fd11ab7c6df18039469e28d1e991e4b7
Ronja Lipsonen <ronja.lipsonen@tuni.fi>
2023-12-04 13:33:13 +0200

    New tests added.

--- src/tests/upperFirst.test.js
+++ src/tests/upperFirst.test.js
@@ -8,4 +8,14 @@ describe('upperFirst', () => {
   it('should keep all chars upper cased', () => {
     expect(upperFirst('FRED')).toBe('FRED');
   });
+
+  it('should keep first chars upper cased', () => {
+    expect(upperFirst('Fred')).toBe('Fred');
+   });
+
+   it('should not convert numbers', () => {
+    expect(upperFirst('0')).toBe('0');
+    expect(upperFirst('1234')).toBe('1234');
+    expect(upperFirst('1a2b3c4')).toBe('1a2b3c4');
+  });
 });
