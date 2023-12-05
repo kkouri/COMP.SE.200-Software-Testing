534cfacbb0323912d06f0ac68a6311da01440344
cbkako <kasperi.kouri@tuni.fi>
2023-12-05 13:51:06 +0200

    Add test cases for get

--- src/tests/get.test.js
+++ src/tests/get.test.js
@@ -1,28 +1,68 @@
 import get from '../get.js';
 
-const object = { a: [{ b: { c: 3 } }] };
+const testObject = { a: [{ b: { c: 3 } }] };
+const testArray = ['a', 'b', 'c'];
+const testString = 'abc';
 
 describe('get', () => {
-  it('should return path value for object', () => {
-    expect(get(object, 'a[0].b.c')).toBe(3);
-    expect(get(object, ['a', '0', 'b', 'c'])).toBe(3);
-    expect(get(object, ['a', '0', 'b', 'c'], 'default')).toBe(3);
+  describe('Accepted input types', () => {
+    it('should return path value for object', () => {
+      expect(get(testObject, 'a[0].b.c')).toBe(3);
+      expect(get(testObject, 'a[0]')).toEqual({ b: { c: 3 } });
+      expect(get(testObject, ['a', '0', 'b', 'c'])).toBe(3);
+      expect(get(testObject, ['a', '0', 'b', 'c'], 'default')).toBe(3);
+    });
+
+    it("should return default as path's value for object", () => {
+      expect(get(testObject, 'a.b.c', 'default')).toBe('default');
+    });
+
+    it('should return undefined as path value for object', () => {
+      expect(get(testObject, 'a.b.c')).toBe(undefined);
+    });
+
+    it('should return path value for array', () => {
+      expect(get(testArray, '0')).toBe('a');
+      expect(get(testArray, '1')).toBe('b');
+      expect(get(testArray, ['0'])).toBe('a');
+      expect(get(testArray, ['1'])).toBe('b');
+    });
+
+    it("should return default as path's value for array", () => {
+      expect(get(testArray, '3', 'default')).toBe('default');
+    });
+
+    it('should return undefined as path value for array', () => {
+      expect(get(testArray, '3')).toBe(undefined);
+    });
+
+    it('should return path value for string', () => {
+      expect(get(testString, '0')).toBe('a');
+      expect(get(testString, '1')).toBe('b');
+      expect(get(testString, ['0'])).toBe('a');
+      expect(get(testString, ['1'])).toBe('b');
+    });
+
+    it("should return default as path's value for string", () => {
+      expect(get(testString, '3', 'default')).toBe('default');
+    });
+
+    it('should return undefined as path value for string', () => {
+      expect(get(testString, '3')).toBe(undefined);
+    });
   });
 
-  it("should return default as path's value for object", () => {
-    expect(get(object, 'a.b.c', 'default')).toBe('default');
-  });
+  describe('Wrong input types', () => {
+    it('should not return path value for number', () => {
+      expect(get(123, '0')).toBe(undefined);
+    });
 
-  it('should return undefined as path value for object', () => {
-    expect(get(object, 'a.b.c')).toBe(undefined);
-  });
+    it('should not return path value for null', () => {
+      expect(get(null, '0')).toBe(undefined);
+    });
 
-  it('should return path value for array', () => {
-    expect(get(['a', 'b', 'c'], '0')).toBe('a');
-    expect(get(['a', 'b', 'c'], '1')).toBe('b');
-  });
-
-  it("should return default as path's value for array", () => {
-    expect(get(['a', 'b', 'c'], '3', 'default')).toBe('default');
+    it('should not return path value for undefined', () => {
+      expect(get(undefined, '0')).toBe(undefined);
+    });
   });
 });
