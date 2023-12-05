150e1d7d8d6170b38600c40b510afb44149d8cb3
cbkako <kasperi.kouri@tuni.fi>
2023-12-05 10:31:42 +0200

    Revert date tests

--- src/tests/filter.test.js
+++ src/tests/filter.test.js
@@ -4,6 +4,15 @@ const strArray = ['a', 'b', 'c', 'd', 'e'];
 const numArray = [1, 2, 3, 4, 5];
 const booleanArray = [true, true, false, true];
 
+const fixedTestDate = new Date(2022, 2, 3);
+const dateArray = [
+  new Date(2024, 1, 1),
+  new Date(2023, 1, 1),
+  fixedTestDate,
+  new Date(2021, 1, 1),
+  new Date(2020, 1, 1),
+];
+
 describe('filter', () => {
   it('should filter by comparing strings', () => {
     expect(filter(strArray, (value) => value > 'b')).toStrictEqual([
@@ -25,14 +34,43 @@ describe('filter', () => {
     expect(filter(numArray, (value) => value > 2)).toStrictEqual([3, 4, 5]);
     expect(filter(numArray, (value) => value <= 2)).toStrictEqual([1, 2]);
     expect(filter(numArray, (value) => value === 2)).toStrictEqual([2]);
-    expect(filter(numArray, (value) => value !== 2)).toStrictEqual([1, 3, 4, 5]);
+    expect(filter(numArray, (value) => value !== 2)).toStrictEqual([
+      1, 3, 4, 5,
+    ]);
   });
 
   it('should filter by comparing booleans', () => {
-    expect(filter(booleanArray, (value) => value)).toStrictEqual([true, true, true]);
+    expect(filter(booleanArray, (value) => value)).toStrictEqual([
+      true,
+      true,
+      true,
+    ]);
     expect(filter(booleanArray, (value) => !value)).toStrictEqual([false]);
     expect(filter(booleanArray, (value) => value > true)).toStrictEqual([[]]);
-    expect(filter(booleanArray, (value) => value < true)).toStrictEqual([false]);
+    expect(filter(booleanArray, (value) => value < true)).toStrictEqual([
+      false,
+    ]);
+  });
+
+  it('should filter by comparing dates', () => {
+    expect(filter(dateArray, (value) => value > fixedTestDate)).toStrictEqual([
+      new Date(2024, 1, 1),
+      new Date(2023, 1, 1),
+    ]);
+    expect(filter(dateArray, (value) => value < fixedTestDate)).toStrictEqual([
+      new Date(2021, 1, 1),
+      new Date(2020, 1, 1),
+    ]);
+
+    expect(filter(dateArray, (value) => value == fixedTestDate)).toEqual([
+      fixedTestDate,
+    ]);
+    expect(filter(dateArray, (value) => value != fixedTestDate)).toStrictEqual([
+      new Date(2024, 1, 1),
+      new Date(2023, 1, 1),
+      new Date(2021, 1, 1),
+      new Date(2020, 1, 1),
+    ]);
   });
 
   it('should filter undefined values', () => {
