8e0d74672280a63ebf1769d5df6bccacb38e6886
cbkako <kasperi.kouri@tuni.fi>
2023-12-06 14:41:31 +0200

    Enhance isSymbol tests

--- src/tests/isSymbol.test.js
+++ src/tests/isSymbol.test.js
@@ -10,7 +10,8 @@ describe('isSymbol', () => {
   });
 
   it('should check that string is not symbol', () => {
-    expect(isSymbol('abc')).toBe(false);
+    expect(isSymbol('')).toBe(false);
+    expect(isSymbol('a')).toBe(false);
     expect(isSymbol('Symbol')).toBe(false);
   });
 
@@ -21,10 +22,16 @@ describe('isSymbol', () => {
     expect(isSymbol(1.1)).toBe(false);
   });
 
-  it('should check that empty value is not symbol', () => {
-    expect(isSymbol(NaN)).toBe(false);
+  it('should check that null is not symbol', () => {
     expect(isSymbol(null)).toBe(false);
+  });
+
+  it('should check that undefined is not symbol', () => {
     expect(isSymbol(undefined)).toBe(false);
+  });
+
+  it('should check that NaN is not symbol', () => {
+    expect(isSymbol(NaN)).toBe(false);
   });
 
   it('should check that boolean is not symbol', () => {
@@ -32,8 +39,13 @@ describe('isSymbol', () => {
     expect(isSymbol(false)).toBe(false);
   });
 
-  it('should check that container is not symbol', () => {
+  it('should check that object is not symbol', () => {
     expect(isSymbol({})).toBe(false);
+    expect(isSymbol({ a: 1 })).toBe(false);
+  });
+
+  it('should check that array is not symbol', () => {
     expect(isSymbol([])).toBe(false);
+    expect(isSymbol([1, 2, 3])).toBe(false);
   });
 });
