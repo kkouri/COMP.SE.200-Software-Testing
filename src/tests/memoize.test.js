87dfc966c48937b67f321a2ece2adf180a772470
cbkako <kasperi.kouri@tuni.fi>
2023-12-09 18:45:59 +0200

    Enhance tests

--- src/tests/memoize.test.js
+++ src/tests/memoize.test.js
@@ -5,7 +5,7 @@ const other = { c: 3, d: 4 };
 const values = memoize(Object.values);
 
 describe('memoize', () => {
-  describe('valid inputs', () => {
+  describe('valid input types', () => {
     it('should return the new memoized function', () => {
       const memoized = memoize(() => {});
       expect(typeof memoized).toBe('function');
@@ -30,24 +30,60 @@ describe('memoize', () => {
     });
   });
 
-  describe('invalid inputs', () => {
-    it('should throw a TypeError if the first argument is not a function', () => {
+  describe('invalid input types', () => {
+    it('should throw a TypeError if func is a number', () => {
       expect(() => memoize(1)).toThrow(TypeError);
+    });
+
+    it('should throw a TypeError if func is a string', () => {
       expect(() => memoize('not a function')).toThrow(TypeError);
+    });
+
+    it('should throw a TypeError if func is null', () => {
       expect(() => memoize(null)).toThrow(TypeError);
+    });
+
+    it('should throw a TypeError if func is undefined', () => {
       expect(() => memoize(undefined)).toThrow(TypeError);
+    });
+
+    it('should throw a TypeError if func is NaN', () => {
       expect(() => memoize(NaN)).toThrow(TypeError);
+    });
+
+    it('should throw a TypeError if func is a boolean', () => {
       expect(() => memoize(true)).toThrow(TypeError);
+    });
+
+    it('should throw a TypeError if func is an object', () => {
       expect(() => memoize({})).toThrow(TypeError);
+    });
+
+    it('should throw a TypeError if func is an array', () => {
       expect(() => memoize([])).toThrow(TypeError);
     });
 
-    it('should throw a TypeError if the second argument is not a function or nullish type', () => {
+    it('should throw a TypeError if resolver is a number', () => {
       expect(() => memoize(() => {}, 1)).toThrow(TypeError);
+    });
+
+    it('should throw a TypeError if resolver is a string', () => {
       expect(() => memoize(() => {}, 'not a function')).toThrow(TypeError);
+    });
+
+    it('should throw a TypeError if resolver is NaN', () => {
       expect(() => memoize(() => {}, NaN)).toThrow(TypeError);
+    });
+
+    it('should throw a TypeError if resolver is a boolean', () => {
       expect(() => memoize(() => {}, true)).toThrow(TypeError);
+    });
+
+    it('should throw a TypeError if resolver is an object', () => {
       expect(() => memoize(() => {}, {})).toThrow(TypeError);
+    });
+
+    it('should throw a TypeError if resolver is an array', () => {
       expect(() => memoize(() => {}, [])).toThrow(TypeError);
     });
   });
